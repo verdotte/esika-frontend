53ab38f59092035c9660fd9c42463bf18dc9e6dd
Eliezer Basubi <eliezer.basubi30@gmail.com>
2021-08-24 21:14:08 +0300

    Add icons to specs tag

--- src/app/modules/__modules__/Tag/index.tsx
+++ src/app/modules/__modules__/Tag/index.tsx
@@ -4,16 +4,23 @@ import ShowWidget from '../ShowWidget';
 interface Props {
   condition?: boolean;
   tag: string | JSX.Element;
+  icon?: null | JSX.Element;
   className?: string;
 }
 
 const defaultProps: Props = {
   condition: true,
   tag: 'Chambre',
+  icon: null,
   className: 'bg-white text-gray-700',
 };
 
-const Tag: FC<Props> = ({ condition = true, tag, className }) => {
+const Tag: FC<Props> = ({
+  condition = true,
+  tag,
+  className,
+  icon,
+}) => {
   if (!tag) {
     return null;
   }
@@ -25,7 +32,10 @@ const Tag: FC<Props> = ({ condition = true, tag, className }) => {
         <div className="h-4 w-12 rounded-full mr-3 bg-gray-200 animate-pulse" />
       }
     >
-      <div className={`p-2 px-3 rounded-full mr-3 ${className}`}>
+      <div
+        className={`p-2 px-3 rounded-full mr-3 flex items-center space-x-2 ${className}`}
+      >
+        {icon}
         <p className="text-xs whitespace-nowrap">{tag}</p>
       </div>
     </ShowWidget>
